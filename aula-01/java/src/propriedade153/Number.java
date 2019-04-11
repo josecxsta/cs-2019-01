25e0a00f737bfc6a9c6f6c6171e2a3ca62ab8177
Your Name <you@example.com>
2019-04-11 19:56:54 -0300

    prop153 em java

---
+++ aula-01/java/src/propriedade153/Number.java
@@ -0,0 +1,40 @@
+package propriedade153;
+
+import java.util.Scanner;
+
+public class Number {
+    private int number;
+
+    public void Number(){
+
+    }
+
+    public void scan(){
+
+        Scanner scan = new Scanner(System.in);
+        this.number = scan.nextInt();
+
+        if(this.number < 0 || this.number > 9999){
+            this.scan();
+        }
+
+    }
+
+    public boolean prop153() {
+
+
+        int c = (number / 100);
+        int du = (number % 100);
+        int d = (du / 10);
+        int u = (du % 10);
+
+        int c3 = (int)Math.pow(c, 3);
+        int d3 = (int)Math.pow(d, 3);
+        int u3 = (int)Math.pow(u, 3);
+
+        int x = c3 + d3 + u3;
+
+        return (x == number);
+
+    }
+}
---
+++ aula-01/java/src/propriedade153/main.java
@@ -0,0 +1,18 @@
+package propriedade153;
+
+import java.util.Scanner;
+
+public class main {
+
+    public static void main (String[] args){
+
+        Number n = new Number();
+
+        n.scan();
+        boolean satisfied = n.prop153();
+
+        System.out.println(satisfied);
+
+    }
+
+}
