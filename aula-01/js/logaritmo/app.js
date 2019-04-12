52da158cf406d0f9d52e61818af7412533276a94
Your Name <you@example.com>
2019-04-11 21:46:09 -0300

    logaritmo em js

---
+++ aula-01/js/logaritmo/app.js
@@ -0,0 +1,40 @@
+var prompt = require('prompt');
+
+prompt.start();
+
+const logaritmo = (n, k) => {
+
+    let i = 2;
+    let e = 1 + n;
+    let numerador = n;
+    let denominador = 1;
+
+    while (i <= k){
+        numerador = numerador * numerador;
+        denominador = denominador * i;
+        e = e + (numerador / denominador);
+        i = i + 1;
+    }
+
+    return e;
+
+}
+
+const askNumber = () => { 
+
+    prompt.get(['n', 'k'], (err, result) => {
+
+        let n = result.n;
+        let k = result.k;
+        
+        if(n >= 1 && k >=2) {
+            console.log(logaritmo(n, k));
+        } else {
+            askNumber();
+        }
+
+    });
+
+}
+
+askNumber();
