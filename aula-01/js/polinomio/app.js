3a7ad8bff098388a36492ec55a12388be8f77ba4
kyriosdata <fabio@inf.ufg.br>
2019-04-25 12:56:56 -0300

    leiaute padrao js

--- aula-01/js/polinomio/app.js
+++ aula-01/js/polinomio/app.js
@@ -4,24 +4,24 @@ prompt.start();
 
 const primo = (n) => {
 
-  let i = 2; 
-  while (i < n){
-    if(n % i == 0) {
-        return false;
-    }
+    let i = 2;
+    while (i < n) {
+        if (n % i == 0) {
+            return false;
+        }
     
-    i++ 
-  } 
-  return true;
+        i++
+    }
+    return true;
 }
 
-const askNumber = () => { 
+const askNumber = () => {
 
     prompt.get(['x'], (err, result) => {
 
         let x = parseInt(result.x);
-        
-        if(x > 1) {
+
+        if (x > 1) {
             console.log(primo(x));
         } else {
             askNumber();
