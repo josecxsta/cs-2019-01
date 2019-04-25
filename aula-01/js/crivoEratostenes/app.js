c5725537f8a1c5cba6f1e915857763d70423c4f7
kyriosdata <fabio@inf.ufg.br>
2019-04-25 12:43:50 -0300

    restrinja o escopo

--- aula-01/js/crivoEratostenes/app.js
+++ aula-01/js/crivoEratostenes/app.js
@@ -3,12 +3,10 @@
 const crivoEratostenes = (a, n) => {
   let i = 2; 
   const limite = Math.sqrt(n);
-  let multiplo;
-  
   while(i <= limite){
 
     if(a[i] == 0){
-      multiplo = i + i 
+      let multiplo = i + i
     
       while(multiplo <= n){
         a[multiplo] = 1;
