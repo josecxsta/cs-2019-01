e98672a4cc3e8d9dc2c86b4c51297bb554036164
kyriosdata <fabio@inf.ufg.br>
2019-04-25 12:53:40 -0300

    comentario

--- aula-01/js/crivoEratostenes/app.js
+++ aula-01/js/crivoEratostenes/app.js
@@ -1,10 +1,17 @@
-
-
+/**
+ * Espero que documente aqui esta função. E também acrescente testes para
+ * verificar se parâmetros estão corretos. Adicionalmente, já percebi sua
+ * preferência por "const crivo em vez de function crivo(a, n)", contudo,
+ * não se pode abrir mão de legibilidade. Arrow function possui seu espaço,
+ * valioso, mas não me parece que este aqui seja o caso.
+ *
+ * @param a
+ * @param n
+ */
 const crivoEratostenes = (a, n) => {
   let i = 2; 
   const limite = Math.sqrt(n);
   while(i <= limite){
-
     if(a[i] == 0){
       let multiplo = i + i
     
@@ -12,7 +19,6 @@ const crivoEratostenes = (a, n) => {
         a[multiplo] = 1;
         multiplo++;
       }
-      
     }
 
     i++;
