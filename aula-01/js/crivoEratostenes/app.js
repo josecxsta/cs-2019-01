3cf0d72cb0566882882d97496a6dc73b4db77691
José da Costa <nunes.j.neto@gmail.com>
2019-04-24 22:49:09 -0300

    update

---
+++ aula-01/js/crivoEratostenes/app.js
@@ -0,0 +1,26 @@
+
+
+const crivoEratostenes = (a, n) => {
+  let i = 2; 
+  let limite = sqrt(n); 
+  let multiplo;
+  
+  while(i <= limite){
+
+    if(a[i] == 0){
+      multiplo = i + i 
+    
+      while(multiplo <= n){
+        a[multiplo] = 1;
+        multiplo++;
+      }
+      
+    }
+
+    i++;
+  }  
+}
+
+
+
+crivoEratostenes([1, 2, 3, 0, 5], 6);
