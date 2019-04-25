a26e086de5eac0da404336eef929dee33d8b3164
ok <email>
2019-04-25 20:59:33 -0300

    melhorias

---
+++ aula-01/java/src/propriedade3025/Main.java
@@ -0,0 +1,27 @@
+package propriedade3025;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int scan() {
+
+        Scanner scan = new Scanner(System.in);
+        int number = scan.nextInt();
+
+        if(number < 0 || number > 9999) {
+            scan();
+        }
+
+        return number;
+    }
+
+    public static void main (String[] args) {
+
+        Number n = new Number();
+
+        System.out.println(n.prop3025(scan()));
+
+    }
+
+}
--- aula-01/java/src/propriedade3025/Number.java
+++ aula-01/java/src/propriedade3025/Number.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 public class Number {
     private int number;
 
-    public void Number() {
-    }
-
-    public void scan() {
-
-        Scanner scan = new Scanner(System.in);
-        this.number = scan.nextInt();
-
-        if(this.number < 0 || this.number > 9999){
-            this.scan();
-        }
-
-    }
-
     public boolean prop3025() {
         int i = (number / 100);
         int j = (number % 100);
