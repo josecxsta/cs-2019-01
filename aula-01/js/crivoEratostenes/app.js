2c4caa8420fe8250200ec710f2d3ac649f156074
ok <email>
2019-04-25 21:03:59 -0300

    comentario

--- aula-01/js/crivoEratostenes/app.js
+++ aula-01/js/crivoEratostenes/app.js
@@ -1,30 +1,28 @@
 /**
- * Espero que documente aqui esta função. E também acrescente testes para
- * verificar se parâmetros estão corretos. Adicionalmente, já percebi sua
- * preferência por "const crivo em vez de function crivo(a, n)", contudo,
- * não se pode abrir mão de legibilidade. Arrow function possui seu espaço,
- * valioso, mas não me parece que este aqui seja o caso.
+ * Funcao para calcular o Crivo de Eratostenes dado um array e um limite.
  *
  * @param a
  * @param n
  */
-const crivoEratostenes = (a, n) => {
+function crivoEratostenes(a, n) {
   let i = 2; 
   const limite = Math.sqrt(n);
-  while(i <= limite){
-    if(a[i] == 0){
+  
+  while(i <= limite) {
+    
+    if(a[i] == 0) {
+      
       let multiplo = i + i
     
-      while(multiplo <= n){
+      while(multiplo <= n) {
         a[multiplo] = 1;
         multiplo++;
       }
+
     }
 
     i++;
   }  
 }
-
-
 
 crivoEratostenes([1, 2, 3, 0, 5], 6);
