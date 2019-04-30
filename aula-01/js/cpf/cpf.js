9cd245f1c6b2543c81685a36bdde4bc23934f0ad
kyriosdata <fabio@inf.ufg.br>
2019-04-30 12:52:17 -0300

    nomes, nao altere parametros como se fossem variaveis locais

--- aula-01/js/cpf/cpf.js
+++ aula-01/js/cpf/cpf.js
@@ -1,24 +1,24 @@
 /**
  * Verifica se o CPF fornecido é válido.
  *
- * @param {string} d Sequência de caracteres correspondente a um CPF.
+ * @param {string} sequencia Sequência de caracteres correspondente a um CPF.
  *
  * @returns {boolean} O valor lógico verdadeiro se...
  *
  * @throws {TypeError} Se não é fornecido argumento ou null ou não é string.
  * @throws {RangeError} Se há caractere que não correspondente a um dígito.
  */
-function cpf(d) {
+function cpf(sequencia) {
 
-    if (d == null || typeof (d) != "string") {
+    if (sequencia == null || typeof (sequencia) != "string") {
         throw new TypeError("argumento deve ser uma string");
     }
 
-    if (d.length != 11) {
+    if (sequencia.length != 11) {
         throw new RangeError("argumento deve possuir 11 caracteres");
     }
 
-    d = convertStringToArray(d);
+    const d = convertStringToArray(sequencia);
 
     const j = d[0] + (2 * d[1]) + (3 * d[2]) + (4 * d[3]) + (5 * d[4]) + (6 * d[5]) + (7 * d[6]) + (8 * d[7]) + (9 * d[8]);
     const k = d[1] + (2 * d[2]) + (3 * d[3]) + (4 * d[4]) + (5 * d[5]) + (6 * d[6]) + (7 * d[7]) + (8 * d[8]) + (9 * d[9]);
