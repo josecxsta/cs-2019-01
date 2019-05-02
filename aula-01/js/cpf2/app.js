92aeea18689e901a6895dd3566e57c022d9ee6a2
José da Costa <nunes.j.neto@gmail.com>
2019-05-02 20:22:53 -0300

    identacao

--- aula-01/js/cpf2/app.js
+++ aula-01/js/cpf2/app.js
@@ -1,12 +1,12 @@
 /**
-* Verifica se o CPF fornecido é válido.
-*
-* @param {string} sequencia Sequência de caracteres correspondente a um CPF.
-*
-* @returns {boolean} O valor lógico verdadeiro se...
-*
-* @throws {TypeError} Se não é fornecido argumento ou null ou não é string.
-* @throws {RangeError} Se há caractere que não correspondente a um dígito.
+ * Verifica se o CPF fornecido é válido.
+ *
+ * @param {string} sequencia Sequência de caracteres correspondente a um CPF.
+ *
+ * @returns {boolean} O valor lógico verdadeiro se...
+ *
+ * @throws {TypeError} Se não é fornecido argumento ou null ou não é string.
+ * @throws {RangeError} Se há caractere que não correspondente a um dígito.
 */
 function cpf2(d){
     
