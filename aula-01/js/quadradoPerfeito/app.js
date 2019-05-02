fc843036bd8e9a5793810292a791c2445a4cd6e1
José da Costa <nunes.j.neto@gmail.com>
2019-05-02 20:00:56 -0300

    quadrado perfeito doc

--- aula-01/js/quadradoPerfeito/app.js
+++ aula-01/js/quadradoPerfeito/app.js
@@ -2,7 +2,18 @@ var prompt = require('prompt');
 
 prompt.start();
 
-const quadradoPerfeito = (n) => {
+/**
+ * Calcula se o numero eh um quadrado perfeito.
+ * @param {number} n numero.
+ * @returns {boolean} se numero eh ou nao quadrado perfeito.
+ * 
+ * @throws {RangeError} se numero for menor do que 1.
+ */
+function quadradoPerfeito(n) {
+
+  if(n < 1) {
+    throw new RangeError("Numero deve ser maior ou igual a 1")
+  }
 
   let i = 1;
   let s = 1;
