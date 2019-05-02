4646f915315adf105fdbad05c38ac0167669617d
José da Costa <nunes.j.neto@gmail.com>
2019-05-02 20:01:18 -0300

    quadrado perfeito doc

--- aula-01/js/quadradoPerfeito/app.js
+++ aula-01/js/quadradoPerfeito/app.js
@@ -12,13 +12,13 @@ prompt.start();
 function quadradoPerfeito(n) {
 
   if(n < 1) {
-    throw new RangeError("Numero deve ser maior ou igual a 1")
+    throw new RangeError("Numero deve ser maior ou igual a 1");
   }
 
   let i = 1;
   let s = 1;
 
-  while(s < n){
+  while(s < n) {
     i = i + 2;
     s = s + 1;
   }
