57c277c7280136ac4c219bdb4917436edfa9c7d1
José da Costa <nunes.j.neto@gmail.com>
2019-05-02 19:53:19 -0300

    resto da divisao doc

--- aula-01/js/restoDivisaoInteira/app.js
+++ aula-01/js/restoDivisaoInteira/app.js
@@ -2,7 +2,20 @@ var prompt = require('prompt');
 
 prompt.start();
 
-const restoDivisao = (x, y) => {
+/**
+ * Calcula o resto da divisao entre dois numeros.
+ * @param {number} x 
+ * @param {number} y
+ * @returns {number} resto da divisao entre x e y.
+ * 
+ * @throws {RangeError} se um dos numeros for menor que zero.
+ */
+function restoDivisao(x, y) {
+    
+    if(y < 0 || x < 0){
+        throw new RangeError('Numeros devem ser maiores que zero.')
+    }
+    
     let s = x;
 
     while(y <= s){
