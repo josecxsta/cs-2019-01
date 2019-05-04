2c6ac11366592b692e1a8b0c0c288d21d0ccf847
José da Costa <nunes.j.neto@gmail.com>
2019-05-04 17:33:16 -0300

    teste 153 e 3025

--- aula-01/js/algoritmos/propriedade3025.js
+++ aula-01/js/algoritmos/propriedade3025.js
@@ -3,11 +3,11 @@
  * @param {number} number numero.
  * @returns {boolean} se numero satisfaz a propriedade 3025.
  * 
- * @throws {RangeError} se numero nao estiver entre 0 e 9999.
+ * @throws {RangeError} se numero nao for inteiro ou nao estiver entre 0 e 9999.
  */
 function propriedade3025 (number) {
 
-    if(number < 0 || number > 9999) {
+    if(number < 0 || number > 9999 || !Number.isInteger(number)) {
         throw new RangeError("Numero deve ser entre 0 e 9999");
     }
 
