cccae0132d64304370f5500c7c49f7db61b7defc
José da Costa <nunes.j.neto@gmail.com>
2019-05-07 21:09:05 -0300

    refatoracao

--- aula-01/js/algoritmos/fatorial.js
+++ aula-01/js/algoritmos/fatorial.js
@@ -3,10 +3,15 @@
  * 
  * @param {number} n numero 
  * @returns {number} f fatorial do numero
- * 
+ * @returns {number} fatorial de n
+ * @throws {TypeError} se argumento nao for um numero.
  * @throws {RangeError} Se o numero informado for menor que 1 
 */
 function fatorial(n){
+
+    if (typeof(n) != "number") {
+        throw new TypeError("argumento deve ser um numero");
+    }
     
     if (n < 1) {
         throw new RangeError("Numero deve ser maior ou igual a 1");
