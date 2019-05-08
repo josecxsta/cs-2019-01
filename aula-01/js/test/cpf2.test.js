c4652c94c50c4ceace5c8b05a73476c838d4f298
José da Costa <nunes.j.neto@gmail.com>
2019-05-08 20:14:14 -0300

    teste cpf

---
+++ aula-01/js/test/cpf2.test.js
@@ -0,0 +1,25 @@
+const funcao = require("../algoritmos/cpf2");
+
+test("CPF gerado satisfaz", () => {
+    expect(funcao("54606451076")).toBe(true);
+});
+
+test("CPF inventado nao satisfaz", () => {
+  expect(funcao("54606451071")).toBe(true);
+});
+
+test("argumento null/undefined", () => {
+    expect(() => { funcao(); }).toThrow();
+});
+
+test("argumento nao numerico", () => {
+    expect(() => { funcao("abcd"); }).toThrow();
+});
+
+test("valor maior que esperado", () => {
+    expect(() => { funcao("1234567899999"); }).toThrow(RangeError);
+});
+
+test("deve ser string", () => {
+    expect(() => funcao(54606451076)).toThrow(TypeError);
+}); 
