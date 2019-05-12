3c7842dba6a139596f574d7fe0a7621a4834130c
José da Costa <nunes.j.neto@gmail.com>
2019-05-11 23:30:52 -0300

    project

--- aula-01/java/src/main/java/com/github/josecxsta/ufg/cs/aula01/package-info.java
+++ aula-01/java/src/main/java/com/github/josecxsta/ufg/cs/aula01/package-info.java
@@ -1,11 +1,4 @@
 /**
- * Pacote do repositório de "exemplo". Contém duas classes
- * apenas: (a) {@link com.github.kyriosdata.exemplo.domain.Calendario} e
- * (b) {@link com.github.kyriosdata.exemplo.application.console.ProgramaCalendario}.
- * Observe que essa última classe apenas ilustra como um
- * programa em Java pode chamar o código da classe
- * relevante ({@link com.github.kyriosdata.exemplo.domain.Calendario}).
- *
  * @see com.github.josecxsta.ufg.cs.aula01.application.Application
  * @see com.github.josecxsta.ufg.cs.aula01.domain.Algoritmos
  */
