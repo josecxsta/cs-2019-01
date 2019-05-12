9df083a7626ec19f20a832b1f3d1c9e5848eb181
José da Costa <nunes.j.neto@gmail.com>
2019-05-11 21:44:33 -0300

    maven project

---
+++ aula-01/java/src/main/java/com/github/josecxsta/ufg/cs/aula01/package-info.java
@@ -0,0 +1,12 @@
+/**
+ * Pacote do repositório de "exemplo". Contém duas classes
+ * apenas: (a) {@link com.github.kyriosdata.exemplo.domain.Calendario} e
+ * (b) {@link com.github.kyriosdata.exemplo.application.console.ProgramaCalendario}.
+ * Observe que essa última classe apenas ilustra como um
+ * programa em Java pode chamar o código da classe
+ * relevante ({@link com.github.kyriosdata.exemplo.domain.Calendario}).
+ *
+ * @see com.github.josecxsta.ufg.cs.aula01.application.Application
+ * @see com.github.josecxsta.ufg.cs.aula01.domain.Algoritmos
+ */
+package com.github.josecxsta.ufg.cs.aula01;
