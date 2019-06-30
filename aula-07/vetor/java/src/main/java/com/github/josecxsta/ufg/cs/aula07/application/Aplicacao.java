4ea28d294372142b864e9aa58e36a821e437b380
José da Costa <josecxsta@gmail.com>
2019-06-30 13:35:02 -0300

    ferramentas e refatoração

--- aula-07/vetor/java/src/main/java/com/github/josecxsta/ufg/cs/aula07/application/Aplicacao.java
+++ aula-07/vetor/java/src/main/java/com/github/josecxsta/ufg/cs/aula07/application/Aplicacao.java
@@ -1,15 +1,23 @@
 package com.github.josecxsta.ufg.cs.aula07.application;
 
-import java.util.Arrays;
-
-import com.github.josecxsta.ufg.cs.aula07.domain.VetorUtils;
 import com.github.josecxsta.ufg.cs.aula07.domain.SorteioUtils;
 
 /**
  * Implementa o método main da aplicação.
  */
-public class Aplicacao {
-    public static void main( String[] args ) {
-        System.out.println( SorteioUtils.sorteio() );
+public final class Aplicacao {
+
+    /**
+     * Para evitar instanciação.
+     */
+    private Aplicacao() {
+    }
+
+    /**
+     * Método main.
+     * @param args não utilizado.
+     */
+    public static void main(final String[] args) {
+        SorteioUtils.sorteio();
     }
 }
