cb4979a3739bccb4a14e43a6385bc27499b2bc9e
José da Costa <josecxsta@gmail.com>
2019-06-30 15:11:33 -0300

    cobertura de testes na aula 7

--- aula-07/vetor/java/src/test/java/com/github/josecxsta/ufg/cs/aula07/domain/SorteioUtilsTest.java
+++ aula-07/vetor/java/src/test/java/com/github/josecxsta/ufg/cs/aula07/domain/SorteioUtilsTest.java
@@ -2,17 +2,14 @@ package com.github.josecxsta.ufg.cs.aula07.domain;
 
 import com.github.josecxsta.ufg.cs.aula07.domain.SorteioUtils;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.HashMap;
-
+import static org.junit.Assert.assertFalse;
 import org.junit.jupiter.api.Test;
 
 public class SorteioUtilsTest {
 
   @Test
   public void sorteio() {
-
+    assertFalse(SorteioUtils.sorteio().getKey() == 12000);
   }
 
 }
