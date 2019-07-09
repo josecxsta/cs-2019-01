e47ee06bf076176402da04ab6e84f82e7fe81b19
José da Costa <josecxsta@gmail.com>
2019-07-09 20:06:34 -0300

    aula 10 iniciada

---
+++ aula-10/src/main/java/com/github/josecxsta/ufg/cs/aula10/application/Application.java
@@ -0,0 +1,17 @@
+package com.github.josecxsta.ufg.cs.aula10.application;
+
+/***
+ * Implementa o método principal.
+ */
+public final class Application {
+
+    /**
+     * Evita a instanciação.
+     */
+    private Application() {
+    }
+
+    public static void main(final String[] args) {
+    }
+
+}
---
+++ aula-10/src/main/java/com/github/josecxsta/ufg/cs/aula10/domain/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @see com.github.josecxsta.ufg.cs.aula10.domain.AlgoritmosUtils
+ * @see com.github.josecxsta.ufg.cs.aula10.domain.CpfUtils
+ * @see com.github.josecxsta.ufg.cs.aula10.domain.DataUtils
+ * @see com.github.josecxsta.ufg.cs.aula10.domain.PropriedadeUtils
+ */
+package com.github.josecxsta.ufg.cs.aula10.domain;
